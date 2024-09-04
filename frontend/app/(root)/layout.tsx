7f76ee2426814f136e50768aa342a436ae52d927
Nikhil-Naikar <nikhil.naikar123@gmail.com>
2024-09-03 22:11:25 -0400

    cp

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -3,7 +3,7 @@
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
    <div className="relative flex flex-col">
-      <main className="relative flex bg-[#101114]">
+      <main className="relative flex bg-[#34b66e]">
         {children}
       </main>
    </div>
--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,10 +1,29 @@
+"use client"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
 const Home = () => {
 
   return (
-    <div className="flex flex-col min-h-screen mt-9 gap-9 text-white ">
-      <h1>Budget Tracker</h1>
+    <div className="flex flex-col min-h-screen mt-10 gap-9 text-white w-full items-center">
+      <section>
+        <Tabs defaultValue="account" className="w-full flex flex-col items-center">
+          <TabsList>
+            <TabsTrigger value="quick-add">Quick Add</TabsTrigger>
+            <TabsTrigger value="detailed-add">Detailed Add</TabsTrigger>
+          </TabsList>
+          <TabsContent value="quick-add">
+            how much? date?
+          </TabsContent>
+          <TabsContent value="detailed-add">
+            how much? which category? details? date?
+          </TabsContent>
+        </Tabs>
+      </section>
+
+      {/* <h1>Quick Sumary - how your doing this month, categories</h1>
+      <h1>recent expenses</h1> */}
     </div>
   )
 }
